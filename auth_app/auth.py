74957d5551b048557fa406cf83e613b3dd5ff0c5
amanaau19 <aman.aau19@gmail.com>
2021-10-04 12:57:49 +0600

    auth_app

---
+++ auth_app/auth.py
@@ -0,0 +1,6 @@
+
+
+def login(user: dict, password: str) -> bool:
+    if user['password'] == password:
+        return True
+    return False
---
+++ auth_app/data.py
@@ -0,0 +1,21 @@
+
+USERS = [
+    {
+        "fullname": "Alex Zolo",
+        "username": "zolo123",
+        "password": "super_pass2021",
+        "logged_in": False
+    },
+    {
+        "fullname": "AJ Meyani",
+        "username": "aj-cool",
+        "password": "aj-boy2021",
+        "logged_in": False
+    },
+    {
+        "fullname": "Shanon Ruder",
+        "username": "shanon-lady",
+        "password": "shaly2021",
+        "logged_in": False
+    }
+]
